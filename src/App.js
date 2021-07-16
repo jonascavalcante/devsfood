70f85394179e3dc87872bd903ae95d503c44fefd
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-16 10:32:33 -0300

    Criação da estrutura base: Container, Menu e PageBody

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useSelector } from 'react-redux';
+
+import { Container, Menu, PageBody } from './AppStyled';
 
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
 export default () => {
-    const name = useSelector(state => state.user.name);
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
+            <Container>
 
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+                <Menu>
+
+                </Menu>
+
+                <PageBody>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>
+
+            </Container>
         </BrowserRouter>
     );
 }
---
+++ src/AppStyled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    background-color: #03AE2A;
+`;
+
+const Menu = styled.div`
+    width: 80px;
+    display: flex;
+    background-color: #136712;
+`;
+
+const PageBody = styled.div`
+    display: flex;
+    flex: 1;
+    background-color: #00990D;
+`;
+
+export { Container, Menu, PageBody };
