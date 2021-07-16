9641bd095a42382e1829a58b3a9a2225d4e40053
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-16 20:48:14 -0300

    Criando rotas privadas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Container, Menu, PageBody } from './AppStyled';
 
 import MenuItem from './components/MenuItem';
+import PrivateRoute from './components/PrivateRoute';
 
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
@@ -22,12 +23,23 @@ export default () => {
 
                 <PageBody>
                     <Switch>
+
                         <Route exact path="/">
                             <HomeScreen />
                         </Route>
+
+                        <PrivateRoute path="/orders">
+                            <div>Pedidos</div>
+                        </PrivateRoute>
+
+                        <PrivateRoute path="/profile">
+                            <div>Perfil</div>
+                        </PrivateRoute>
+
                         <Route path="/tela2/:nome">
                             <Tela2Screen />
                         </Route>
+
                     </Switch>
                 </PageBody>
 
