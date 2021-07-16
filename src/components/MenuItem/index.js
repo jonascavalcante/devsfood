025bb61d842153addf202142a803245806a2c6db
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-16 10:57:03 -0300

    Usando useHistory para evitar recarregamento

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { LinkArea, LinkIcon } from './styled';
 
 const MenuItem = ({ icon, link }) => {
 
+    const history = useHistory();
+
+    const handleLinkClick = (e) => {
+        e.preventDefault();
+        history.push( link );
+    }
+
     return (
-        <LinkArea href={link}>
+        <LinkArea href={link} onClick={handleLinkClick}>
             <LinkIcon src={icon} />
         </LinkArea>        
     );
