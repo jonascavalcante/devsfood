6b51c976497913bb362695039d918705fe968035
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-16 20:45:57 -0300

    Configurando token

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,12 +1,15 @@
 const initialState = {
+    token: '',
     name: 'Teste'
 };
 
 export default (state = initialState, action) => {
-    switch(action.type) {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+        case 'SET_TOKEN':
+            return { ...state, token: action.payload.token };
         case 'SET_NAME':
-            return {...state, name: action.payload.name};
-        break;
+            return { ...state, name: action.payload.name };
     }
 
     return state;
