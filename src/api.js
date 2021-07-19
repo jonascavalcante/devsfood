3b55c6f2e2e4b0c7cddd7e4c8e7db957de9b6426
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-19 09:51:32 -0300

    Configurando requisição com getProducts

--- src/api.js
+++ src/api.js
@@ -5,5 +5,10 @@ export default {
         const res = await fetch(BASE + '/categories');
         const json = await res.json();
         return json;
+    },
+    getProducts: async () => {
+        const res = await fetch(BASE + '/products');
+        const json = await res.json();
+        return json;
     }
 }
