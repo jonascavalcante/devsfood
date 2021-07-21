b49ea0db85d41bb3e48b51a0fac4aa94f763f169
jonascavalcante <jonascavalcantejc@gmail.com>
2021-07-21 20:19:56 -0300

    Estilizando o Cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import { CartArea, CartBody, CartHeader, CartIcon, CartText } from './styled';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { CartArea, CartBody, CartHeader, CartIcon, CartText, ProductInfoArea, ProductItem, ProductName, ProductPhoto, ProductPrice, ProductQtIcon, ProductQtText, ProductQuantityArea, ProductsArea } from './styled';
 
 const Cart = () => {
+
+    const products = useSelector(state => state.cart.products);
+
+    const [show, setShow] = useState(true);
+
+    const handleCartClick = () => {
+        setShow(!show);
+    }
 
     return (
         <CartArea>
 
-            <CartHeader>
+            <CartHeader onClick={handleCartClick}>
                 <CartIcon src="/assets/cart.png" />
-                <CartText>Meu carrinho (x)</CartText>
+                <CartText>Meu carrinho ({products.length})</CartText>
+                {show &&
+                    <CartIcon src="/assets/down.png" />
+                }
             </CartHeader>
 
-            <CartBody>
+            <CartBody show={show}>
+                <ProductsArea>
+                    {products.map((item, index) => (
+                        <ProductItem key={index}>
 
+                            <ProductPhoto src={item.image} />
+
+                            <ProductInfoArea>
+                                <ProductName>{item.name}</ProductName>
+                                <ProductPrice>R$ {item.price.toFixed(2)}</ProductPrice>
+                            </ProductInfoArea>
+
+                            <ProductQuantityArea>
+
+                                <ProductQtIcon
+                                    src="/assets/minus.png"
+                                />
+
+                                <ProductQtText>{item.qt}</ProductQtText>
+
+                                <ProductQtIcon
+                                    src="/assets/plus.png"
+                                />
+
+                            </ProductQuantityArea>
+
+                        </ProductItem>
+                    ))}
+                </ProductsArea>
             </CartBody>
 
         </CartArea>
--- src/components/Cart/styled.js
+++ src/components/Cart/styled.js
@@ -20,7 +20,8 @@ const CartHeader = styled.div`
 `;
 
 const CartBody = styled.div`
-
+    display: ${props => props.show ? 'block' : 'none'};
+    color: #fff;
 `;
 
 const CartIcon = styled.img`
@@ -30,8 +31,55 @@ const CartIcon = styled.img`
 `;
 
 const CartText = styled.div`
+    flex: 1;
     font-size: 17px;
     color: #fff;
 `;
 
-export { CartArea, CartHeader, CartBody, CartIcon, CartText };
+const ProductsArea = styled.div`
+
+`;
+
+const ProductItem = styled.div`
+    margin: 10px;
+    display: flex;
+    gap: 10px;
+`;
+
+const ProductPhoto = styled.img`
+    width: 64px;
+    height: auto;
+    border-radius: 10px;
+`;
+
+const ProductInfoArea = styled.div`
+    flex: 1;
+`;
+
+const ProductName = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+`;
+
+const ProductPrice = styled.div`
+    font-size: 13px;
+`;
+
+const ProductQuantityArea = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
+
+const ProductQtIcon = styled.img`
+    width: 13px;
+    height: auto;
+    cursor: pointer;
+`;
+
+const ProductQtText = styled.div`
+    font-size: 13px;
+    font-weight: bold;
+`;
+
+export { CartArea, CartHeader, CartBody, CartIcon, CartText, ProductsArea, ProductItem, ProductPhoto, ProductInfoArea, ProductName, ProductPrice, ProductQuantityArea, ProductQtIcon, ProductQtText };
